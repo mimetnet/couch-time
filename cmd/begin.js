0d9417e1db3aca8dca2f6cd0f3413030fa22daa1
Matthew Metnetsky <matthew@cowarthill.com>
2013-04-16 17:30:49 -0400

    cmd/begin.js :: use reltime to modify date and use vim's done argument

--- cmd/begin.js
+++ cmd/begin.js
@@ -1,8 +1,9 @@
 var vim = require('../lib/vim.js'),
     config = require('../lib/config.js'),
-    insert = require('../lib/insert.js');
+    insert = require('../lib/insert.js'),
+    reltime = require('reltime');
 
-function begin(cfg, date, row) {
+function begin(cfg, date, row, done) {
     insert(cfg.nano(), row, function(error, ret) {
         if (error) {
             console.error('Failed to save new record:', error);
@@ -15,11 +16,15 @@ function begin(cfg, date, row) {
         // updated _rev on old record.
         config.save(cfg, function() {
             console.log('Begin @', date);
+
+            if ('function' === typeof(done)) {
+                done();
+            }
         });
     });
 }
 
-function save(opts, msg) {
+function save(opts, msg, done) {
     config.load(function(cfg) {
         var date = new Date(), row = {
             _id: date.getTime().toString(),
@@ -27,6 +32,11 @@ function save(opts, msg) {
             end: null,
             tag: (opts.tag || ((cfg.last && cfg.last.tag)? cfg.last.tag : null))
         };
+
+        if (opts.when) {
+            date = reltime.parse(date, opts.when);
+            row._id = date.getTime().toString();
+        }
 
         // last `begin` hasn't been stopped
         if ('object' === typeof(cfg.last) && 'number' !== typeof(cfg.last.end)) {
@@ -38,22 +48,22 @@ function save(opts, msg) {
                 } else {
                     cfg.last = last;
 
-                    begin(cfg, date, row);
+                    begin(cfg, date, row, done);
                 }
             });
         } else {
-            begin(cfg, date, row);
+            begin(cfg, date, row, done);
         }
     });
 }
 
 module.exports = function(args, opts) {
     if (0 === args.length) {
-        vim(function(error, data) {
+        vim(function(error, data, done) {
             if (error) {
                 console.error(error);
             } else if ('string' === typeof(data)) {
-                save(opts, data);
+                save(opts, data, done);
             }
         });
     } else {
