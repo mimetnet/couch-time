ea7e62f4a5f09c4ca461a964e55140050240706d
Matthew Metnetsky <matthew@cowarthill.com>
2013-04-30 23:08:28 -0400

    cmd/clear.js :: use loaded cfg passed into us

--- cmd/clear.js
+++ cmd/clear.js
@@ -1,29 +1,27 @@
 var config = require('../lib/config.js');
 
-module.exports = function(args, opts) {
+module.exports = function(args, opts, cfg) {
     var date = new Date();
 
-    config.load(function(cfg) {
-        if (cfg.last) {
-            var last = cfg.last;
+    if (cfg.last) {
+        var last = cfg.last;
 
-            cfg.nano().destroy(last._id, last._rev, function (err, ret, headers) {
-                if (err) {
-                    if (404 === err.status_code) {
-                        console.error('Current record not found:', last.msg);
-                    } else {
-                        console.error('Unknown Error', error);
-                    }
-                } else if (true === ret.ok) {
-                    delete cfg.last;
-
-                    config.save(cfg, function() {
-                        console.log('Cleared: ', last.msg);
-                    });
+        cfg.nano().destroy(last._id, last._rev, function (err, ret, headers) {
+            if (err) {
+                if (404 === err.status_code) {
+                    console.error('Current record not found:', last.msg);
+                } else {
+                    console.error('Unknown Error', error);
                 }
-            });
-        } else {
-            console.log('You have nothing to clear.... have you begun something?');
-        }
-    });
+            } else if (true === ret.ok) {
+                delete cfg.last;
+
+                config.save(cfg, function() {
+                    console.log('Cleared: ', last.msg);
+                });
+            }
+        });
+    } else {
+        console.log('You have nothing to clear.... have you begun something?');
+    }
 };
