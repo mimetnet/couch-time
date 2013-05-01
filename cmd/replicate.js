ef8bfa4cb0220aa440039e16e270f0d39c2ed9bd
Matthew Metnetsky <matthew@cowarthill.com>
2013-04-30 23:08:40 -0400

    cmd/replicate.js :: use loaded cfg passed into us

--- cmd/replicate.js
+++ cmd/replicate.js
@@ -1,37 +1,35 @@
 var config = require('../lib/config.js');
 
-module.exports = function(args, opts) {
-    config.load(function(cfg) {
-        var db = cfg.nano({timeout: 100000});
+module.exports = function(args, opts, cfg) {
+    var db = cfg.nano({timeout: 100000});
 
-        args.forEach(function(url) {
-            db.replicate(url, {create_target:false}, function(err, res, headers) {
-                if (err) {
-                    if ('db_not_found' === err.error) {
-                        console.error('Database not found:', err.reason);
-                        console.error(' >> Please make sure it exists and that it is secure');
-                    } else {
-                        console.error(err);
-                    }
+    args.forEach(function(url) {
+        db.replicate(url, {create_target:false}, function(err, res, headers) {
+            if (err) {
+                if ('db_not_found' === err.error) {
+                    console.error('Database not found:', err.reason);
+                    console.error(' >> Please make sure it exists and that it is secure');
                 } else {
-                    console.log('Replicated to "' + url + '"');
-
-                    if (true === res.no_changes)
-                        console.log(' >> No changes');
-
-                    if (Array.isArray(res.history)) {
-                        res.history.forEach(function(row) {
-                            console.log(' >> Read:', row.docs_read);
-                            console.log(' >> Written:', row.docs_written);
-                            console.log(' >> Failures:', row.doc_write_failures);
-                        });
-                    }
+                    console.error(err);
                 }
+            } else {
+                console.log('Replicated to "' + url + '"');
 
-                if (config.auth(cfg, headers)) {
-                    config.save(cfg);
+                if (true === res.no_changes)
+                    console.log(' >> No changes');
+
+                if (Array.isArray(res.history)) {
+                    res.history.forEach(function(row) {
+                        console.log(' >> Read:', row.docs_read);
+                        console.log(' >> Written:', row.docs_written);
+                        console.log(' >> Failures:', row.doc_write_failures);
+                    });
                 }
-            });
+            }
+
+            if (config.auth(cfg, headers)) {
+                config.save(cfg);
+            }
         });
     });
 };
