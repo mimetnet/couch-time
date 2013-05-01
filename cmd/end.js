c925491e23bb6cb9226d2cf2d3a8d0312929a0fa
Matthew Metnetsky <matthew@cowarthill.com>
2013-04-30 23:08:02 -0400

    cmd/end.js :: use loaded cfg passed into us

--- cmd/end.js
+++ cmd/end.js
@@ -2,31 +2,29 @@ var config = require('../lib/config.js'),
     insert = require('../lib/insert.js'),
     reltime = require('reltime');
 
-module.exports = function(args, opts) {
+module.exports = function(args, opts, cfg) {
     var date = new Date();
 
     if (opts.when) {
         date = reltime.parse(date, opts.when);
     }
 
-    config.load(function(cfg) {
-        if (cfg.last) {
-            cfg.last.end = date.getTime();
+    if (cfg.last) {
+        cfg.last.end = date.getTime();
 
-            insert(cfg.nano(), cfg.last, function(error, ret, headers) {
-                if (error) {
-                    console.error(error);
-                } else {
-                    cfg.last = ret;
+        insert(cfg.nano(), cfg.last, function(error, ret, headers) {
+            if (error) {
+                console.error(error);
+            } else {
+                cfg.last = ret;
 
-                    config.auth(cfg, headers);
-                    config.save(cfg, function() {
-                        console.log('End @', date);
-                    });
-                }
-            });
-        } else {
-            console.log('You have nothing to end.... have you begun something?');
-        }
-    });
+                config.auth(cfg, headers);
+                config.save(cfg, function() {
+                    console.log('End @', date);
+                });
+            }
+        });
+    } else {
+        console.log('You have nothing to end.... have you begun something?');
+    }
 };
